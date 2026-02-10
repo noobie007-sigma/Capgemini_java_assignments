aea1b8cde82572f2c16d667f33cd43839ca9b840
noobie007-sigma <sukjais7120@gmail.com>
2026-02-10 16:56:31 +0530

    added missing programs

---
+++ assignment2-9-Feb/insertionSort.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class insertionSort {
+
+    public static void sort(int[] arr) {
+        
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 2, 9, 1, 5, 6 };
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
